ca81887672efc9e77fa040f16db1461c42c4246d
Mokhtar tarek Samara <mokhtartareksamara@gmail.com>
2026-03-02 07:21:33 +0000

    File changes

--- src/components/radar/RadarMap.jsx
+++ src/components/radar/RadarMap.jsx
@@ -2,8 +2,6 @@ import { useEffect, useRef } from "react";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "leaflet.markercluster/dist/MarkerCluster.css";
-import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import "leaflet.markercluster";
 
 const SEVERITY_COLORS = {
