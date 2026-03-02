d16d7225c5f37b5101a81ba8d34d5a760f24508b
Mokhtar tarek Samara <mokhtartareksamara@gmail.com>
2026-03-02 08:42:16 +0000

    File changes

--- src/components/radar/TopBar.jsx
+++ src/components/radar/TopBar.jsx
@@ -84,7 +84,6 @@ export default function TopBar({ eventCount, onAddEvent }) {
                     <span>{info.name}</span>
                   </button>
                 ))}
-              </div>
             </div>
           )}
         </div>
