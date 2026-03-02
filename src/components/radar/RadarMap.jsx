cc4099a0ff186b34c3401e198c73af22e9342bc6
Mokhtar tarek Samara <mokhtartareksamara@gmail.com>
2026-03-02 08:18:25 +0000

    File changes

--- src/components/radar/RadarMap.jsx
+++ src/components/radar/RadarMap.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
-import { MapContainer, TileLayer, useMap, Marker, Tooltip, Polygon, useMapEvents } from "react-leaflet";
+import { MapContainer, TileLayer, useMap, Marker, Tooltip, Polygon } from "react-leaflet";
 import { useLang } from "../LanguageContext";
+import GeofenceManager from "./GeofenceManager";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
