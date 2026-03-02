fade0ac927d7443513e1b93fa8b7bdf7c98e32e5
Mokhtar tarek Samara <mokhtartareksamara@gmail.com>
2026-03-02 06:37:44 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import PulseFeed from "../components/radar/PulseFeed";
 import RadarMap from "../components/radar/RadarMap";
 import EventDetailPanel from "../components/radar/EventDetailPanel";
 import SmartDigestPanel from "../components/radar/SmartDigestPanel";
+import TrendAnalysisPanel from "../components/radar/TrendAnalysisPanel";
 import AddEventModal from "../components/radar/AddEventModal";
 import { AlertStack } from "../components/radar/AlertNotificationStack";
 
