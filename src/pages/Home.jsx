f9814c292fa5c89ff768b1be14b66d9192a7d839
Mokhtar tarek Samara <mokhtartareksamara@gmail.com>
2026-03-02 08:09:48 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ import AddEventModal from "../components/radar/AddEventModal";
 import { AlertStack } from "../components/radar/AlertNotificationStack";
 import PredictiveAlertEngine from "../components/radar/PredictiveAlertEngine";
 import PredictiveAlertToast from "../components/radar/PredictiveAlertToast";
+import { useLang } from "../components/LanguageContext";
 
 export default function Home() {
   const [events, setEvents] = useState([]);
